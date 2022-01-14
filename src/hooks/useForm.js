d1518f6ab6249284979a84d1c86f73a7a5ebb519
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-01-14 08:52:20 -0800

    PlantList showing

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,10 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+const useForm = (initValues) => {
+  const [values, setValues] = useState(initValues);
+
+  return [values];
+}
+
+export default useForm;
