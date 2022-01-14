5a3be71026f2d6e365a86cee57e4e2b702a0b3a3
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-01-14 09:41:21 -0800

    Created custom hook and is working in form

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,10 +1,28 @@
 // write your custom hook here to control your checkout form
 import { useState } from "react";
 
-const useForm = (initValues) => {
-  const [values, setValues] = useState(initValues);
+const initialValue = {
+  firstName: "",
+  lastName: "",
+  address: "",
+  city: "",
+  state: "",
+  zip: "",
+};
 
-  return [values];
+const useForm = () => {
+  const [values, setValues] = useState(initialValue);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  return [ values, showSuccessMessage, handleChanges, handleSubmit ];
 }
 
 export default useForm;
